d1ed073c00cfa29358dc54be9af43ad18e98e282
emiliolatorre <emiliolatorreguerra@gmail.com>
2024-05-17 12:14:15 +0200

    Ejercicio corregido
    correccion de clase

--- scripts.js
+++ scripts.js
@@ -1,23 +1,29 @@
-console.log(document.title);
+
 
 //1. Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".
-let titulo = document.getElementById('gen-1');
-titulo.textContent = 'Generasión 1 Pokimon';
+const titulo = document.getElementById('gen-1');
+titulo.innerHTML = 'Generasión 1 Pokimon';
 
 //2. Cambia el color de fondo de la primera generación de Pokimon.
-titulo.style.backgroundColor = 'blue';
+const generación = document.getElementById('gen-1').nextElementSibling;
+generación.style.backgroundColor = 'blue';
 
 //3. Imprime por consola la URL de la página.
-console.log()
-meta property="og:url
+const url = document.location.href
+console.log(url);
 
 //4. Imprime por consola el dominio de la página.
+const domain = document.location.domain;
+console.log(domain);
 
 //5. Imprime todos los nodos de imagen.
-let imagenes = document.getElementsByTagName('img');
+let imagenes = document.querySelectorAll('img');
 console.log(imagenes);
 
 //6. Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
-for (let i=0; i<imagenes.length; i++) {
-    imagenes[i].src ='https://media.giphy.com/media/2v170e71aanfi/giphy.gif';
-};
+imagenes.forEach(img => {
+    img.src ='https://media.giphy.com/media/2v170e71aanfi/giphy.gif'
+})
+
+console.log(document.title);
+console.log(document.title);
